f5d1afe6456731951b73e26072eedcb5acd059ae
K-Wu <11816012+K-Wu@users.noreply.github.com>
2022-07-24 02:01:27 +0000

    one comment line

--- hetero_edgesoftmax/include/DGLHackKernel/HGTExperimental.h
+++ hetero_edgesoftmax/include/DGLHackKernel/HGTExperimental.h
@@ -225,6 +225,7 @@ __device__ __forceinline__ static void func512_32_mysgemm_exec(int m, int n, int
 __device__ __forceinline__ int binary_search(int num_elements, int *__restrict__ arr, int target)
 {
     int lo = 0, hi = num_elements;
+    // find element in arr[i] where i in [lo, hi)
     // This below check covers all cases , so need to check
     // for mid=lo-(hi-lo)/2
     while (hi - lo > 1)
