2d3d62ee4b6d3e700552a2e427135a6c7ea79ec9
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2022-10-18 21:02:00 -0500

    impl util loader func without test

--- hetero_edgesoftmax/python/utils/__init__.py
+++ hetero_edgesoftmax/python/utils/__init__.py
@@ -1,2 +1,3 @@
 #!/usr/bin/env python3
 from load_from_npy import *
+from sparse_mat_conversion import *
--- hetero_edgesoftmax/python/utils/load_from_npy.py
+++ hetero_edgesoftmax/python/utils/load_from_npy.py
@@ -1,6 +1,34 @@
 #!/usr/bin/env python3
 import numpy as np
 import os
+import torch as th
+
+def create_mydgl_graph_csr(row_ptr, col_idx, rel_types, eids):
+    row_ptr = th.from_numpy(row_ptr).long()
+    col_idx = th.from_numpy(col_idx).long()
+    rel_types = th.from_numpy(rel_types).long()
+    eids = th.from_numpy(eids).long()
+    g = dict()
+    g["original"] = dict()
+    g["original"]["row_ptr"] = row_ptr
+    g["original"]["col_idx"] = col_idx
+    g["original"]["rel_types"] = rel_types
+    g["original"]["eids"] = eids
+    return g
+
+
+def create_mydgl_graph_coo(edge_srcs, edge_dsts, edge_etypes, edge_referential_eids):
+    edge_srcs = th.from_numpy(edge_srcs).long()
+    edge_dsts = th.from_numpy(edge_dsts).long()
+    edge_etypes = th.from_numpy(edge_etypes).long()
+    edge_referential_eids = th.from_numpy(edge_referential_eids).long()
+
+    g = dict()
+    g["original"] = dict()
+    g["original"]["srcs"] = edge_srcs
+    g["original"]["dsts"] = edge_dsts
+    g["original"]["etypes"] = edge_etypes
+    g["original"]["eids"] = edge_referential_eids
 
 
 def pyutils_load_fb15k237(dataset_path_prefix, sorted, sorted_by_srcs, transposed):
