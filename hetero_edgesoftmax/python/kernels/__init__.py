8f51f00c212ec9367473dc8a14800f2415088732
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2022-11-30 07:04:02 -0600

    rm deprecated files in python.kernels

--- hetero_edgesoftmax/python/kernels/__init__.py
+++ hetero_edgesoftmax/python/kernels/__init__.py
@@ -4,9 +4,3 @@ import torch
 torch.ops.load_library("../build/hetero_edgesoftmax/libtorch_hetero_edgesoftmax.so")
 torch.ops.torch_hetero_edgesoftmax.build_debug_info()
 K = torch.ops.torch_hetero_edgesoftmax
-# from .rgcn_kernels import *
-# from .hgt_kernels import *
-# from .util_kernels import *
-# from .rgat_kernels import *
-# from .rgnn_kernels import *
-# from .gat_kernels import *
