67a9db98afcf276df17dfc016448e470f1bc853e
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2023-06-04 13:01:40 -0500

    upd script

--- hetero_edgesoftmax/experiments/profile_all.sh
+++ hetero_edgesoftmax/experiments/profile_all.sh
@@ -1,6 +1,6 @@
 # source this file to execute
 declare -a MODELS=("HGT.train" "RGAT.train")
-declare -a CompactFlag=("--compact_as_of_node_flag" "")
+declare -a CompactFlag=("--compact_as_of_node_flag" "" "--compact_as_of_node_flag --compact_direct_indexing_flag")
 declare -a MulFlag=("--multiply_among_weights_first_flag" "")
 declare -a Datasets=("aifb" "mutag" "bgs" "am" "mag" "wikikg2" "fb15k" "biokg")
 
@@ -8,7 +8,7 @@ for m in ${MODELS[@]}
 do
     for d in ${Datasets[@]}
     do
-        for c_idx in {0..1}
+        for c_idx in {0..2}
         do
             for mf_idx in {0..1}
             do
@@ -19,7 +19,7 @@ do
                 #then
                 #    continue
                 #fi
-                nv-nsight-cu-cli --export "$m.$d.$mf.$c" --force-overwrite --target-processes all --kernel-name-base function --kernel-name regex:HET --launch-skip-before-match 0 --section ComputeWorkloadAnalysis --section InstructionStats --section LaunchStats --section MemoryWorkloadAnalysis --section MemoryWorkloadAnalysis_Chart --section MemoryWorkloadAnalysis_Tables --section Occupancy --section SchedulerStats --section SourceCounters --section SpeedOfLight --section SpeedOfLight_HierarchicalSingleRooflineChart --section WarpStateStats --sampling-interval auto --sampling-max-passes 5 --sampling-buffer-size 33554432 --profile-from-start 1 --cache-control all --clock-control base python -m python.$m -d $d --num_layers 1  --full_graph_training --num_classes 64 --n_infeat 64 $c $mf -e 1 --no_warm_up
+                nv-nsight-cu-cli --export "$m.$d.$mf.${c//[[:blank:]]/}" --force-overwrite --target-processes all --kernel-name-base function --kernel-name regex:HET --launch-skip-before-match 0 --section ComputeWorkloadAnalysis --section InstructionStats --section LaunchStats --section MemoryWorkloadAnalysis --section MemoryWorkloadAnalysis_Chart --section MemoryWorkloadAnalysis_Tables --section Occupancy --section SchedulerStats --section SourceCounters --section SpeedOfLight --section SpeedOfLight_HierarchicalSingleRooflineChart --section WarpStateStats --sampling-interval auto --sampling-max-passes 5 --sampling-buffer-size 33554432 --profile-from-start 1 --cache-control all --clock-control base python -m python.$m -d $d --num_layers 1  --full_graph_training --num_classes 64 --n_infeat 64 $c $mf -e 1 --no_warm_up
             done
         done
     done
