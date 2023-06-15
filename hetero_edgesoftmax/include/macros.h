9af4bb9042356c445b2024410f1307f0a8c4e6fb
K-Wu <kunww@google.com>
2023-06-15 04:26:29 +0000

    tuning sgemm

--- hetero_edgesoftmax/include/macros.h
+++ hetero_edgesoftmax/include/macros.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #if CUDA_ARCHS == 86  // RTX 3090
-// TODO: use the reg tiling flag instead of equal_1 checking
 #define MY_SGEMM_LAUNCH_BOUNDS                        \
   __launch_bounds__(RIGHT_REG_TILED_FLAG ? 128 : 256, \
                     RIGHT_REG_TILED_FLAG ? 6 : 3)
@@ -18,27 +17,23 @@
                               : WORK_BLOCK_SIZE##SUFFIX##_X / 2; \
   constexpr int THREADING_BLOCK_SIZE##SUFFIX##_Y =               \
       REG_TILING_FLAG##SUFFIX ? 4 : WORK_BLOCK_SIZE##SUFFIX##_Y / 2;
-// TODO: specify thread factor larger than 1
 
 #elif CUDA_ARCHS == 80  // A100
-// TODO: use the reg tiling flag instead of equal_1 checking
-
-#define MY_SGEMM_LAUNCH_BOUNDS                       \
-  __launch_bounds__(RIGHT_REG_TILED_FLAG ? 64 : 256, \
-                    RIGHT_REG_TILED_FLAG ? 8 : 3)
+#define MY_SGEMM_LAUNCH_BOUNDS                        \
+  __launch_bounds__(RIGHT_REG_TILED_FLAG ? 128 : 256, \
+                    RIGHT_REG_TILED_FLAG ? 6 : 3)
 
 #define MY_SGEMM_GRID_CONFIG(SUFFIX)                             \
   constexpr int WORK_BLOCK_SIZE##SUFFIX##_X =                    \
       REG_TILING_FLAG##SUFFIX ? 32 : 32;                         \
   constexpr int WORK_BLOCK_SIZE##SUFFIX##_Y =                    \
-      REG_TILING_FLAG##SUFFIX ? 8 : 32;                          \
+      REG_TILING_FLAG##SUFFIX ? 32 : 32;                         \
   constexpr int WORK_BLOCK_SIZE##SUFFIX##_K =                    \
       REG_TILING_FLAG##SUFFIX ? 8 : 32;                          \
   constexpr int THREADING_BLOCK_SIZE##SUFFIX##_X =               \
       REG_TILING_FLAG##SUFFIX ? WORK_BLOCK_SIZE##SUFFIX##_X      \
                               : WORK_BLOCK_SIZE##SUFFIX##_X / 2; \
   constexpr int THREADING_BLOCK_SIZE##SUFFIX##_Y =               \
-      REG_TILING_FLAG##SUFFIX ? 2 : WORK_BLOCK_SIZE##SUFFIX##_Y / 2;
-// TODO: specify thread factor larger than 1
+      REG_TILING_FLAG##SUFFIX ? 4 : WORK_BLOCK_SIZE##SUFFIX##_Y / 2;
 
 #endif
