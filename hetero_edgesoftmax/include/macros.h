1f8172a7ebbf7cdc8c967a5ed21365483fe78e10
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2023-07-08 11:57:02 -0500

    recover performance from debugging

--- hetero_edgesoftmax/include/macros.h
+++ hetero_edgesoftmax/include/macros.h
@@ -2,11 +2,11 @@
 
 #if CUDA_ARCHS == 86  // RTX 3090
 // TODO: use the reg tiling flag instead of equal_1 checking
-#define MY_SGEMM_LAUNCH_BOUNDS                        \
-  __launch_bounds__(RIGHT_REG_TILED_FLAG ? 128 : 256, \
+#define MY_SGEMM_LAUNCH_BOUNDS                       \
+  __launch_bounds__(RIGHT_REG_TILED_FLAG ? 64 : 256, \
                     RIGHT_REG_TILED_FLAG ? 6 : 3)
-#define NO_SCATTER_GATHER_LAUNCH_BOUNDS               \
-  __launch_bounds__(RIGHT_REG_TILED_FLAG ? 128 : 256, \
+#define NO_SCATTER_GATHER_LAUNCH_BOUNDS              \
+  __launch_bounds__(RIGHT_REG_TILED_FLAG ? 64 : 256, \
                     RIGHT_REG_TILED_FLAG ? 5 : 3)
 
 #define MY_SGEMM_GRID_CONFIG(SUFFIX)                             \
@@ -20,7 +20,7 @@
       REG_TILING_FLAG##SUFFIX ? WORK_BLOCK_SIZE##SUFFIX##_X      \
                               : WORK_BLOCK_SIZE##SUFFIX##_X / 2; \
   constexpr int THREADING_BLOCK_SIZE##SUFFIX##_Y =               \
-      REG_TILING_FLAG##SUFFIX ? 4 : WORK_BLOCK_SIZE##SUFFIX##_Y / 2;
+      REG_TILING_FLAG##SUFFIX ? 2 : WORK_BLOCK_SIZE##SUFFIX##_Y / 2;
 // TODO: specify thread factor larger than 1
 
 #elif CUDA_ARCHS == 80  // A100
