170f08aab01b450af035c4215c1ebeaf50c81fb6
K-Wu <kunww@google.com>
2023-07-12 04:09:45 +0000

    upd scripts

--- hetero_edgesoftmax/utils/benchmark_all.sh
+++ hetero_edgesoftmax/utils/benchmark_all.sh
@@ -3,4 +3,4 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source "${DIR}/_do_all_cases.sh"
 do_all_cases "misc/artifacts/benchmark_all_$(date +%Y%m%d%H%M)" \
   "" \
-  ">>\"\${OUTPUT_DIR}/\$m.64.64.1.\$d.\$mf.\${c//[[:blank:]]/}.result.log\" 2>&1"
+  ">>\"\${OUTPUT_DIR}/\$m.\$d.\$mf.\${c//[[:blank:]]/}.\$dim.\$dim.1.result.log\" 2>&1"
--- hetero_edgesoftmax/utils/trace_nsys_all.sh
+++ hetero_edgesoftmax/utils/trace_nsys_all.sh
@@ -2,5 +2,5 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source "${DIR}/_do_all_cases.sh"
 do_all_cases "misc/artifacts/nsys_trace_$(date +%Y%m%d%H%M)" \
-  "nsys profile --force-overwrite true -o \"\$OUTPUT_DIR/\$m.\$d.\$mf.\${c//[[:blank:]]/}\" " \
+  "nsys profile --force-overwrite true -o \"\$OUTPUT_DIR/\$m.\$d.\$mf.\${c//[[:blank:]]/}.\$dim.\$dim.1\" " \
   "-e 1 --no_warm_up"
