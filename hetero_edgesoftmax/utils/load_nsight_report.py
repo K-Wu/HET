0d0defd9859f6449df9ad9b6db09963a848e4f4c
K-Wu <kunww@google.com>
2023-07-24 02:21:44 +0000

    deving roofline, cache reprot

--- hetero_edgesoftmax/utils/load_nsight_report.py
+++ hetero_edgesoftmax/utils/load_nsight_report.py
@@ -2,6 +2,7 @@ import os
 import re
 from .detect_pwd import is_pwd_het_dev_root, run_once
 from functools import lru_cache
+from typing import Tuple
 
 
 @lru_cache(maxsize=None)
@@ -39,6 +40,7 @@ def extract_csv_from_nsys_cli_output(nsys_cli_output: str) -> "list[list[str]]":
 def load_nsys_report(filename: str, report_name: str) -> "list[list[str]]":
     """Load a report from a nsys report file."""
     assert nsys_exists(), "nsys is not installed"
+    assert os.path.exists(filename), f"{filename} does not exist"
     nsys_cli_output: str = os.popen(
         f"nsys stats -f csv -r {report_name} {filename}"
     ).read()
@@ -46,6 +48,7 @@ def load_nsys_report(filename: str, report_name: str) -> "list[list[str]]":
 
 
 NCU_DETAILS_COLUMN_IDX = {
+    "ID": 0,
     "Kernel Name": 4,
     "Section Name": 11,
     "Metric Name": 12,
@@ -57,18 +60,142 @@ NCU_DETAILS_COLUMN_IDX = {
 }
 
 
-def extract_ncu_values_from(
+def construct_raw_column_idx(
+    header: "list[str]", columns: "set[str]"
+) -> "dict[str, int]":
+    result = {}
+    for column in columns:
+        result[column] = header.index(column)
+    return result
+
+
+def extract_ncu_values_from_raws(
+    nsys_details_csv: "list[list[str]]",
+    raw_metrics: "set[str]" = {
+        "smsp__sass_thread_inst_executed_op_fadd_pred_on.sum.per_cycle_elapsed",
+        "smsp__sass_thread_inst_executed_op_fmul_pred_on.sum.per_cycle_elapsed",
+        "derived__smsp__sass_thread_inst_executed_op_ffma_pred_on_x2",
+        "smsp__cycles_elapsed.avg.per_second",
+        "derived__l1tex__lsu_writeback_bytes_mem_lg.sum.per_second",
+        "l1tex__m_xbar2l1tex_read_bytes.sum.per_second",
+        "dram__bytes.sum.per_second",
+        "derived__sm__sass_thread_inst_executed_op_ffma_pred_on_x2",
+        "smsp__cycles_elapsed.avg.per_second",
+    },
+) -> "list[list[str]]":
+    header: list[str] = nsys_details_csv[0]
+    units: list[str] = nsys_details_csv[1]
+    assert header[0] == "ID", f"header[0] = {header[0]} != ID"
+    assert header[4] == "Kernel Name", f"header[4] = {header[4]} != Kernel Name"
+    NCU_DETAILS_COLUMN_IDX: dict[str, int] = construct_raw_column_idx(
+        header,
+        raw_metrics,
+    )
+    results: list[list[str]] = [
+        ["ID", "Pretty Name", "Kernel Name"] + [key for key in NCU_DETAILS_COLUMN_IDX],
+        ["", "", ""]
+        + [units[NCU_DETAILS_COLUMN_IDX[key]] for key in NCU_DETAILS_COLUMN_IDX],
+    ]
+    for row in nsys_details_csv[2:]:  # Skip header and units
+        results.append(
+            [
+                row[0],  # ID
+                extract_func_name_from_signature(row[4]),  # Kernel Name
+                row[4],  # Kernel Name
+            ]
+            + [row[NCU_DETAILS_COLUMN_IDX[key]] for key in NCU_DETAILS_COLUMN_IDX]
+        )
+    return results
+
+
+def duplicate_metrics_exists(metrics_and_units: "set[Tuple[str, str]]") -> bool:
+    """
+    Assert there are no two metrics with the same name (i.e., with different units)
+    """
+    metrics: set[str] = set()
+    for metric, unit in metrics_and_units:
+        if metric in metrics:
+            return True  # f"Duplicate metric {metric} with different units"
+        metrics.add(metric)
+    return False
+
+
+def consolidate_ncu_details(metric_per_row: "list[list[str]]") -> "list[list[str]]":
+    """
+    The original output from extract_ncu_values_from_details shows one metric in each row,
+    this function consolidate it so that each row show all metrics of a kernel instance,
+    similar to the ncu raw csv output
+    """
+    header: list[str] = metric_per_row[0]
+    name_columns: list[str] = ["ID", "Pretty Name", "Kernel Name"]
+    name_columns_idx: dict[str, int] = {key: header.index(key) for key in name_columns}
+    metric_columns: list[str] = ["Metric Name", "Metric Unit", "Metric Value"]
+    metric_columns_idx: dict[str, int] = {
+        key: header.index(key) for key in metric_columns
+    }
+    results_dict: dict[Tuple[str, str, str], dict[str, Tuple[str, str]]] = {}
+
+    metrics_and_units: set[Tuple[str, str]] = {}
+    for row in metric_per_row[1:]:
+        key = (
+            row[name_columns_idx["ID"]],
+            row[name_columns_idx["Pretty Name"]],
+            row[name_columns_idx["Kernel Name"]],
+        )
+        if key not in results_dict:
+            results_dict[key] = dict()
+        assert (
+            row[metric_columns_idx["Metric Name"]] not in results_dict[key]
+        ), f"Duplicate metric name {row[metric_columns_idx['Metric Name']]} for {key}"
+        results_dict[key][row[metric_columns_idx["Metric Name"]]] = (
+            row[metric_columns_idx["Metric Unit"]],
+            row[metric_columns_idx["Metric Value"]],
+        )
+        metrics_and_units.add(
+            (
+                row[metric_columns_idx["Metric Name"]],
+                row[metric_columns_idx["Metric Unit"]],
+            )
+        )
+
+    assert not duplicate_metrics_exists(
+        metrics_and_units
+    ), f"Duplicate metrics exist: {metrics_and_units}"
+    results: list[list[str]] = [
+        name_columns + [ele[0] for ele in metrics_and_units],
+        [""] * len(name_columns) + [ele[1] for ele in metrics_and_units],
+    ]
+    for key in results_dict:
+        row = list(key)
+        for metric in metric_columns:
+            # print("Warning: ", metric, "not in", key)
+            row += results_dict[key][metric]
+        results.append(row)
+    return results
+
+
+def extract_ncu_values_from_details(
     nsys_details_csv: "list[list[str]]",
     metric_names: "set[str]" = {
+        "ID",
         "L2 Cache Throughput",
         "L1/TEX Cache Throughput",
         "DRAM Throughput",
         "Memory Throughput",
     },
-):
-    header = nsys_details_csv[0]
+) -> "list[list[str]]":
+    header: list[str] = nsys_details_csv[0]
 
-    results = []
+    results: list[list[str]] = [
+        [
+            "ID",
+            "Pretty Name",
+            "Kernel Name",
+            "Metric Name",
+            "Metric Unit",
+            "Metric Value",
+        ]
+    ]
     for key in NCU_DETAILS_COLUMN_IDX:
         assert (
             header[NCU_DETAILS_COLUMN_IDX[key]] == key
@@ -77,6 +204,7 @@ def extract_ncu_values_from(
         if row[NCU_DETAILS_COLUMN_IDX["Metric Name"]] in metric_names:
             results.append(
                 [
+                    row[NCU_DETAILS_COLUMN_IDX["ID"]],
                     extract_func_name_from_signature(
                         row[NCU_DETAILS_COLUMN_IDX["Kernel Name"]]
                     ),
@@ -93,9 +221,9 @@ def load_csv_from_multiline_string(csv_string: str) -> "list[list[str]]":
     # ncu output is multiline csv where each cell value is wrapped by double quotes.
     # We need to remove the double quotes and split the string by comma.
     result = []
-    lines = csv_string.split("\n")
+    lines: list[str] = csv_string.split("\n")
     for line in lines:
-        line = line.strip()
+        line: str = line.strip()
         if len(line) == 0:
             continue
         elif line.startswith('"'):
@@ -112,6 +240,7 @@ def load_csv_from_multiline_string(csv_string: str) -> "list[list[str]]":
 def load_ncu_report(filename: str, page_name: str) -> "list[list[str]]":
     """Load a report from a ncu report file."""
     assert ncu_exists(), "ncu is not installed"
+    assert os.path.exists(filename), f"{filename} does not exist"
     nsys_cli_output: str = os.popen(
         f"ncu --page {page_name} --csv  --import {filename}"
     ).read()
@@ -139,23 +268,34 @@ def extract_func_name_from_signature(func_signature: str) -> str:
 if __name__ == "__main__":
     assert is_pwd_het_dev_root(), "Please run this script at het_dev root"
 
-    print(
-        load_nsys_report(
-            "../third_party/OthersArtifacts/graphiler/graphiler_hgt_fb15k.nsys-rep",
-            "cuda_gpu_trace",
-        )
-    )
-    print(
-        load_nsys_report(
-            "../third_party/OthersArtifacts/graphiler/graphiler_hgt_fb15k.nsys-rep",
-            "cuda_gpu_trace,nvtx_sum,osrt_sum,cuda_api_sum,cuda_gpu_kern_sum,cuda_gpu_mem_size_sum,cuda_gpu_mem_time_sum",
-        )
-    )
-
-    print(
-        extract_ncu_values_from(
-            load_ncu_report(
-                "../third_party/OthersArtifacts/graphiler/hgt_biokg.ncu-rep", "details"
+    try:
+        print(
+            load_nsys_report(
+                "../third_party/OthersArtifacts/graphiler/graphiler_hgt_fb15k.nsys-rep",
+                "cuda_gpu_trace",
             )
         )
-    )
+    except Exception as e:
+        print("Error occurred", e)
+
+    try:
+        print(
+            load_nsys_report(
+                "../third_party/OthersArtifacts/graphiler/graphiler_hgt_fb15k.nsys-rep",
+                "cuda_gpu_trace,nvtx_sum,osrt_sum,cuda_api_sum,cuda_gpu_kern_sum,cuda_gpu_mem_size_sum,cuda_gpu_mem_time_sum",
+            )
+        )
+    except Exception as e:
+        print("Error occurred2", e)
+
+    try:
+        print(
+            extract_ncu_values_from_details(
+                load_ncu_report(
+                    "../third_party/OthersArtifacts/graphiler/graphiler.bgs.HGT.64.64.ncu-rep",
+                    "details",
+                )
+            )
+        )
+    except Exception as e:
+        print("Error occurred3", e)
--- hetero_edgesoftmax/utils/upload_cache_and_roofline.py
+++ hetero_edgesoftmax/utils/upload_cache_and_roofline.py
@@ -1,14 +1,18 @@
 from .detect_pwd import is_pwd_het_dev_root
 from .upload_benchmark_results import ask_subdirectory
 from .load_nsight_report import (
-    extract_ncu_values_from,
+    extract_ncu_values_from_details,
+    extract_ncu_values_from_raws,
+    load_ncu_report,
     extract_csv_from_nsys_cli_output,
 )
-from .upload_benchmark_results import NameCanonicalizer
+from typing import Union
+from .upload_benchmark_results import NameCanonicalizer, update_gspread
+import os
 
 
 def extract_info_from_ncu(filename) -> "list[str]":
-    # model_name.dataset_name.mul_flag.compact_flag.resul
+    # model_name.dataset_name.mul_flag.compact_flag.ncu-rep
     return NameCanonicalizer.to_list(
         filename[: filename.rfind(".ncu-rep")],
         "model.dataset.flag_mul.flag_compact.ax_in.ax_out.ax_head",
@@ -16,14 +20,39 @@ def extract_info_from_ncu(filename) -> "list[str]":
 
 
 def extract_info_from_nsys(filename) -> "list[str]":
-    # model_name.dataset_name.mul_flag.compact_flag.resul
+    # model_name.dataset_name.mul_flag.compact_flag.nsys-rep
     return NameCanonicalizer.to_list(
         filename[: filename.rfind(".nsys-rep")],
         "model.dataset.flag_mul.flag_compact.ax_in.ax_out.ax_head",
     )
 
 
+def extract_memory_from_ncu_folder(path) -> "list[list[Union[float, str, int]]]":
+    results = []
+    for filename in os.listdir(path):
+        if filename.endswith(".ncu-rep"):
+            name_and_info: list[str] = extract_info_from_ncu(filename)
+            func_and_mem = extract_ncu_values_from_details(
+                load_ncu_report(os.path.join(path, filename), "details")
+            )
+            results.append([name_and_info + f_ for f_ in func_and_mem])
+    return results
+
+
+def extract_roofline_from_ncu_folder(path) -> "list[list[Union[float, str, int]]]":
+    results = []
+    for filename in os.listdir(path):
+        if filename.endswith(".ncu-rep"):
+            name_and_info: list[str] = extract_info_from_ncu(filename)
+            func_and_mem = extract_ncu_values_from_raws(
+                load_ncu_report(os.path.join(path, filename), "raw")
+            )
+            results.append([name_and_info + f_ for f_ in func_and_mem])
+    return results
+
+
 if __name__ == "__main__":
     assert is_pwd_het_dev_root(), "Please run this script at het_dev root"
     dirname = ask_subdirectory("misc/artifacts", "ncu_breakdown_")
+
     pass
