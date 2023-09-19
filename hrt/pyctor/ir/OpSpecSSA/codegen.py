839f0d0e93f566c483afab0566f19ca12b0f430f
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2023-09-19 13:16:52 -0500

    refactor

--- hrt/pyctor/ir/OpSpecSSA/codegen.py
+++ hrt/pyctor/ir/OpSpecSSA/codegen.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from ...utils import is_pwd_het_dev_root
+
+assert (
+    is_pwd_het_dev_root()
+), "Please run this script from het_dev root directory."
 
 env = Environment(
     loader=FileSystemLoader(searchpath="./pyctor/ir/OpSpecSSA/templates/"),
---
+++ hrt/pyctor/tests/test_detect_pwd.py
@@ -0,0 +1,4 @@
+from ..utils import is_pwd_het_dev_root
+
+if __name__ == "__main__":
+    print(is_pwd_het_dev_root())
