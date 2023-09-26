2d42e873b31079ab14f675b61b851092d8380cc9
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2023-09-26 01:08:45 -0500

    upd playground

--- hrt/pyctor/tests/test_render_jinja.py
+++ hrt/pyctor/tests/test_render_jinja.py
@@ -3,6 +3,9 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 incode_template = """{# This jinja template is a copy of hrt/pyctor/tests/test.jinja that shows how to use jinja template to generate code. #}
 res = {{ matmul_autodiff_func_name }}({{ matmul_autodiff_func_args }})
+# try curly
+{{ '{' }}this is interesting{{ '}' }}
+{{ '{{' }}this is interesting{{ '}}' }}
 """
 
 if __name__ == "__main__":
@@ -20,6 +23,8 @@ if __name__ == "__main__":
             ),
         ).strip()  # Remove newline trailing and ahead caused by comments
     )
+    # Printed text:
+    # res = test_func(a = a,b = b)
 
     template2 = env.from_string(incode_template)
     print(
@@ -30,3 +35,7 @@ if __name__ == "__main__":
             ),
         ).strip()  # Remove newline trailing and ahead caused by comments
     )
+    # Printed text:
+    # res = test_func(a = a,b = b)
+    # # try curly
+    # {this is interesting}
