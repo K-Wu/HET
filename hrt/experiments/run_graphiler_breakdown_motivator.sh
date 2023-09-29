ee4385e23928c1f8ac17c6e98986844541690ca5
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2023-09-29 02:53:30 -0500

    collect sm metrics

--- hrt/experiments/run_graphiler_breakdown_motivator.sh
+++ hrt/experiments/run_graphiler_breakdown_motivator.sh
@@ -7,8 +7,8 @@ assert_in_het_dev_root
 OUTPUT_DIR=misc/artifacts/motivator_graphiler_breakdown_`date +%Y%m%d%H%M`
 mkdir -p ${OUTPUT_DIR}
 echo ${OUTPUT_DIR}
-nsys profile --gpu-metrics-device=all --force-overwrite true -o ${OUTPUT_DIR}/graphiler.mutag_HGT.bg.breakdown python3 ../third_party/OthersArtifacts/graphiler/examples/HGT/HGT.py mutag 64 64
-nsys profile --gpu-metrics-device=all --force-overwrite true -o ${OUTPUT_DIR}/graphiler.fb15k_HGT.bg.breakdown python3 ../third_party/OthersArtifacts/graphiler/examples/HGT/HGT.py fb15k 64 64
+nsys profile --gpu-metrics-device=all --gpu-metrics-frequency=50000 --force-overwrite true -o ${OUTPUT_DIR}/graphiler.mutag_HGT.bg.breakdown python3 ../third_party/OthersArtifacts/graphiler/examples/HGT/HGT.py mutag 64 64
+nsys profile --gpu-metrics-device=all --gpu-metrics-frequency=50000 --force-overwrite true -o ${OUTPUT_DIR}/graphiler.fb15k_HGT.bg.breakdown python3 ../third_party/OthersArtifacts/graphiler/examples/HGT/HGT.py fb15k 64 64
 
-nsys profile --gpu-metrics-device=all --force-overwrite true -o ${OUTPUT_DIR}/graphiler.mutag_RGAT.bg.breakdown python3 ../third_party/OthersArtifacts/graphiler/examples/RGAT/RGAT.py mutag 64 64
-nsys profile --gpu-metrics-device=all --force-overwrite true -o ${OUTPUT_DIR}/graphiler.fb15k_RGAT.bg.breakdown python3 ../third_party/OthersArtifacts/graphiler/examples/RGAT/RGAT.py fb15k 64 64
+nsys profile --gpu-metrics-device=all --gpu-metrics-frequency=50000 --force-overwrite true -o ${OUTPUT_DIR}/graphiler.mutag_RGAT.bg.breakdown python3 ../third_party/OthersArtifacts/graphiler/examples/RGAT/RGAT.py mutag 64 64
+nsys profile --gpu-metrics-device=all --gpu-metrics-frequency=50000 --force-overwrite true -o ${OUTPUT_DIR}/graphiler.fb15k_RGAT.bg.breakdown python3 ../third_party/OthersArtifacts/graphiler/examples/RGAT/RGAT.py fb15k 64 64
--- hrt/experiments/run_het_breakdown_motivator.sh
+++ hrt/experiments/run_het_breakdown_motivator.sh
@@ -1,7 +1,12 @@
 # Use cuda_gpu_kern_sum and aggregate to get Hector kernels consumed time and PyTorch kernels consumed time, and then substract it from the total time to get others' time
 # Alternatively, we can still use cuda_gpu_kern_sum to get Hecto kernels consumed time and PyTorch kernels consumed time. But we count all the kernels time in the training range and substract it from the range duration to get others' time
-nsys profile --force-overwrite true -o mutag_rgat.bg.breakdown python -m python.RGAT.train -d mutag --num_layers 1 --full_graph_training --num_classes 64 --n_infeat 64 -e 1 --no_warm_up
-nsys profile --force-overwrite true -o fb15k_rgat.bg.breakdown python -m python.RGAT.train -d fb15k --num_layers 1 --full_graph_training --num_classes 64 --n_infeat 64 -e 1 --no_warm_up
 
-nsys profile --force-overwrite true -o mutag_hgt.bg.breakdown python -m python.HGT.train -d mutag --num_layers 1 --full_graph_training --num_classes 64 --n_infeat 64 -e 1 --no_warm_up
-nsys profile --force-overwrite true -o fb15k_hgt.bg.breakdown python -m python.HGT.train -d fb15k --num_layers 1 --full_graph_training --num_classes 64 --n_infeat 64 -e 1 --no_warm_up
+OUTPUT_DIR=misc/artifacts/motivator_het_breakdown_`date +%Y%m%d%H%M`
+mkdir -p ${OUTPUT_DIR}
+echo ${OUTPUT_DIR}
+
+nsys profile --gpu-metrics-device=all --gpu-metrics-frequency=20000 --force-overwrite true -o ${OUTPUT_DIR}/mutag_rgat.bg.breakdown python -m python.RGAT.train -d mutag --num_layers 1 --full_graph_training --num_classes 64 --n_infeat 64 -e 5 --no_warm_up
+nsys profile --gpu-metrics-device=all --gpu-metrics-frequency=20000 --force-overwrite true -o ${OUTPUT_DIR}/fb15k_rgat.bg.breakdown python -m python.RGAT.train -d fb15k --num_layers 1 --full_graph_training --num_classes 64 --n_infeat 64 -e 5 --no_warm_up
+
+nsys profile --gpu-metrics-device=all --gpu-metrics-frequency=20000 --force-overwrite true -o ${OUTPUT_DIR}/mutag_hgt.bg.breakdown python -m python.HGT.train -d mutag --num_layers 1 --full_graph_training --num_classes 64 --n_infeat 64 -e 5 --no_warm_up
+nsys profile --gpu-metrics-device=all --gpu-metrics-frequency=20000 --force-overwrite true -o ${OUTPUT_DIR}/fb15k_hgt.bg.breakdown python -m python.HGT.train -d fb15k --num_layers 1 --full_graph_training --num_classes 64 --n_infeat 64 -e 5 --no_warm_up
