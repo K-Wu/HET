4c2bc34c3ca7b9ed712f8f2738b27e4b2fa54ba5
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2023-10-13 02:36:08 -0500

    mv playground to as submodule

--- hrt/misc/playground/echo_script_path.sh
+++
@@ -1,6 +0,0 @@
-
-# Get the directory of the script
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-echo $DIR
-# Source a file relative to the script
-# source "$DIR/relative/path/to/file"
